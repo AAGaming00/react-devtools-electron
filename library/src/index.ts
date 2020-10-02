5a98ec8aa77f6587675352435aefa2e082b7d4bc
AAGaming <aa@bigdumb.gq>
2020-10-02 11:54:31 -0400

    Update index.ts

--- library/src/index.ts
+++ library/src/index.ts
@@ -2,12 +2,4 @@ import { app } from 'electron'
 
 import { addReactDevToolsExtension, removeReactDevToolsExtension } from './utils'
 
-if (app.isReady()) {
-	addReactDevToolsExtension()
-} else {
-	app.once('ready', () => {
-		addReactDevToolsExtension()
-	})
-}
-
 export { addReactDevToolsExtension, removeReactDevToolsExtension }
